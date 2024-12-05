4e5e470b0e68d89b59c9e3a0d33c66a5a9429ced
David Yoon <davidyoon@hanwha.com>
2024-12-06 08:13:12 +0900

    [RA-25] 입력 화면 추가
    * 분 입력 화면 추가
    * dismiss 액션 추가

--- IntervalAlarm/Feature/Add/CustomInputFeature.swift
+++ IntervalAlarm/Feature/Add/CustomInputFeature.swift
@@ -21,12 +21,13 @@ struct CustomInputFeature {
         case setMinutes(String)
     }
     
+    @Dependency(\.dismiss) var dismiss
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .didTapBack:
-                
-                return .none
+                return .run { _ in await dismiss() }
             case .setMinutes(let minutes):
                 state.minutes = minutes
                 return .none
@@ -43,36 +44,35 @@ struct CustomInputView: View {
     
     var body: some View {
         WithPerceptionTracking {
-            
-        }
-        VStack(alignment: .leading) {
-            HStack(spacing: 0.0) {
-                Button(action: {
-                    // TODO: - dismiss action
-                }, label: {
-                    Images.icArrowBack24.swiftUIImage
-                })
-                .padding(.horizontal, 8.0)
-                Text("Manual Settings")
-                    .font(Fonts.Pretendard.bold.swiftUIFont(size: 24.0))
-                    .foregroundStyle(.black100)
-                    .padding(.vertical, 8.0)
+            VStack(alignment: .leading) {
+                HStack(spacing: 0.0) {
+                    Button(action: {
+                        store.send(.didTapBack)
+                    }, label: {
+                        Images.icArrowBack24.swiftUIImage
+                    })
+                    .padding(.horizontal, 8.0)
+                    Text("Manual Settings")
+                        .font(Fonts.Pretendard.bold.swiftUIFont(size: 24.0))
+                        .foregroundStyle(.black100)
+                        .padding(.vertical, 8.0)
+                    Spacer()
+                }
+                .padding(.horizontal, 20.0)
+                .padding(.top, 30.0)
+                
+                HStack {
+                    Spacer()
+                    TextField("분", text: $store.minutes.sending(\.setMinutes))
+                        .font(Fonts.Pretendard.medium.swiftUIFont(size: 72.0))
+                        .keyboardType(.numberPad)
+                        .frame(height: 86.0)
+                        .padding(.top, 80.0)
+                        .fixedSize()
+                    Spacer()
+                }
                 Spacer()
             }
-            .padding(.horizontal, 20.0)
-            .padding(.top, 30.0)
-            
-            HStack {
-                Spacer()
-                TextField("분", text: $store.minutes.sending(\.setMinutes))
-                    .font(Fonts.Pretendard.medium.swiftUIFont(size: 72.0))
-                    .keyboardType(.numberPad)
-                    .frame(height: 86.0)
-                    .padding(.top, 80.0)
-                    .fixedSize()
-                Spacer()
-            }
-            Spacer()
         }
     }
     
