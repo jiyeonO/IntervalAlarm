20ce441b5c08ac3f9c51654fc4d7ed2e13bb9ccc
David Yoon <davidyoon@hanwha.com>
2024-12-20 14:58:15 +0900

    [RA-28] WithPerceptionTracking 추가
    * WithPerceptionTracking 누락 추가

--- IntervalAlarm/Extension/View+Extension.swift
+++ IntervalAlarm/Extension/View+Extension.swift
@@ -5,14 +5,17 @@
 //  Created by 오지연 on 12/3/24.
 //
 import SwiftUI
+import ComposableArchitecture
 
 extension View {
     
     func measureHeight() -> some View {  // TODO: - 이름 변경할 필요가 있어 보임(presentationDetents 까지 포함되어 있기 때문)
         background(
             GeometryReader { geometry in
-                Color.clear
-                    .presentationDetents([.height(geometry.size.height)])
+                WithPerceptionTracking {
+                    Color.clear
+                        .presentationDetents([.height(geometry.size.height)])
+                }
             }
         )
     }
@@ -20,11 +23,13 @@ extension View {
     func measureHeight(_ height: @escaping (CGFloat) -> Void) -> some View {
         background(
             GeometryReader { geometry in
-                Color.clear
-                    .preference(key: HeightPreferenceKey.self, value: geometry.size.height)
-                    .onPreferenceChange(HeightPreferenceKey.self) { newHeight in
-                        height(newHeight)
-                    }
+                WithPerceptionTracking {
+                    Color.clear
+                        .preference(key: HeightPreferenceKey.self, value: geometry.size.height)
+                        .onPreferenceChange(HeightPreferenceKey.self) { newHeight in
+                            height(newHeight)
+                        }
+                }
             }
         )
     }
