088353ab0e9eb27ff6313422f60565de8e7cf7a9
jiyeonO <jiyeonO>
2025-02-11 17:31:18 +0900

    리뷰 반영 - offset제거 - extension UINavigation 추가

--- IntervalAlarm/Feature/Common/ViewModifier/SwipeModifier.swift
+++ IntervalAlarm/Feature/Common/ViewModifier/SwipeModifier.swift
@@ -10,26 +10,13 @@ struct SwipeModifier: ViewModifier {
     
     let action: (() -> Void)
     
-    @State private var offset: CGFloat = 0
-    
     func body(content: Content) -> some View {
         content
-            .offset(x: offset)
-            .animation(.interactiveSpring(), value: offset)
             .gesture(
                 DragGesture()
-                    .onChanged { gesture in
-                        if gesture.translation.width > 0 {
-                            offset = gesture.translation.width
-                        }
-                    }
                     .onEnded { gesture in
                         if gesture.translation.width > UIScreen.main.bounds.width * 0.3 {
                             action()
-                        } else {
-                            withAnimation(.interactiveSpring()) {
-                                offset = 0
-                            }
                         }
                     }
             )
